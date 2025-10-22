de1eb857860b797064a3379e3918894920434479
JoelFaldin <joelfaldin@gmail.com>
2025-10-22 13:51:16 -0300

    refactor: adding date and time to greeting

--- daily_greetings/greet.c
+++ daily_greetings/greet.c
@@ -4,16 +4,38 @@
 int main(int argc, char *argv[])
 {
     time_t now;
+    struct tm *clock;
+    int hour;
+    char time_string[64];
 
     time(&now);
+    clock = localtime(&now);
+    hour = clock -> tm_hour;
 
-    if (argc < 2) {
-        printf("Hello, bro!\n");
-        printf("It is %ld\n", now);
-        printf("%s", ctime(&now));
+    if (hour > 0 && hour < 4) {
+        printf("You still awake, fam! Go to bed.");
     } else {
-        printf("Hello, %s!\n", argv[1]);
+        printf("Good ");
+    
+        if (hour < 12) {
+            printf("morning");
+        } else if (hour < 17) {
+            printf("afternoon");
+        } else {
+            printf("evening");
+        }
+    
+        if (argc > 1) {
+            printf(", %s!", argv[1]);
+        } else {
+            printf(", bro!");
+        }
     }
+
+    strftime(time_string, 64, "Today is %A, %B %d, %Y%nIt is %r%n", clock);
+    printf("\n%s", time_string);
+
+    putchar('\n');
 
     return 0;
 }

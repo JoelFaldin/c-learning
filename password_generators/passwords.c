ed16476c7ead63b08d31781deb18419da22dadb8
JoelFaldin <joelfaldin@gmail.com>
2025-11-27 23:07:54 -0300

    feat: adding improved password generator exercise

---
+++ password_generators/passwords.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+
+char *number(void) {
+    static char n[2];
+
+    n[0] = rand() % 10 + '0';
+    n[1] = '\0';
+
+    return n;
+}
+
+char *symbol(void) {
+    char sym[8] = "!@#$%*_-";
+    static char s[2];
+
+    s[0] = sym[rand() % 8];
+    s[1] = '\0';
+
+    return s;
+}
+
+char *add_word(void) {
+    char *vocabulary[] = {
+        "Orange", "Grape", "Apple", "Banana",
+        "coffee", "tea", "juice", "beverage",
+        "happY", "grumpY", "bashfuL", "sleepY"
+    };
+
+    int r;
+
+    r = rand() % 12;
+    return vocabulary[r];
+}
+
+int main() {
+    char password[32];
+
+    srand((unsigned)time(NULL));
+
+    password[0] = '\0';
+
+    strcpy(password, add_word());
+    strcat(password, number());
+    strcat(password, add_word());
+    strcat(password, symbol());
+    strcat(password, add_word());
+
+    printf("%s\n", password);
+
+    return 0;
+}

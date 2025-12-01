ae638e97494a2144fd4408375d462c53cbf53bfd
JoelFaldin <joelfaldin@gmail.com>
2025-12-01 14:29:09 -0300

    feat: add trimming function to wide_in

--- unicode/wide_in.c
+++ unicode/wide_in.c
@@ -2,15 +2,30 @@
 #include <wchar.h>
 #include <locale.h>
 
+wchar_t *trimming(wchar_t *s) {
+    int len = wcslen(s);
+    int x;
+
+    wchar_t *new_s;
+
+    new_s = s;
+    new_s[len - 1] = '\0';
+
+    return new_s;
+}
+
 int main() {
     const int size = 32;
-    wchar_t input[size];
+    wchar_t input[size], *new_input;
 
     setlocale(LC_CTYPE, "en_US.UTF-8");
 
     wprintf(L"Type some fancy text: ");
     fgetws(input, size, stdin);
-    wprintf(L"You typed: '%ls'\n", input);
+
+    new_input = trimming(input);
+
+    wprintf(L"You typed: '%ls'\n", new_input);
 
     return 0;
 }

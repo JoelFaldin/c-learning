06bbc1ec41c2f2fca74c749f7ed7d6d79b48dc88
JoelFaldin <joelfaldin@gmail.com>
2025-12-01 13:58:43 -0300

    feat: add wide character input exercises

---
+++ unicode/wscanf.c
@@ -0,0 +1,19 @@
+#include <wchar.h>
+#include <locale.h>
+
+int main() {
+    const wchar_t pound = 0xa3;
+    int quantity;
+    float total;
+
+    setlocale(LC_CTYPE, "en_US.UTF-8");
+
+    wprintf(L"How many crisps do u want? ");
+    wscanf(L"%d", &quantity);
+
+    total = quantity * 1.4;
+
+    wprintf(L"That'll be %lc%.2f\n", pound, total);
+
+    return 0;
+}

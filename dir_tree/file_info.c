b7334dc819d3b76a5dc5ffa30710198112babd61
JoelFaldin <joelfaldin@gmail.com>
2025-12-04 22:24:49 -0300

    feat: adding read-write-execute details on file info program

--- dir_tree/file_info.c
+++ dir_tree/file_info.c
@@ -38,13 +38,68 @@ int main(int argc, char *argv[]) {
     
     printf("Type and mode: %X\n", fs.st_mode);
 
-    if (S_ISREG(fs.st_mode)) {
-        printf("%s is a regular file!\n", filename);
+    // if (S_ISREG(fs.st_mode)) {
+    //     printf("%s is a regular file!\n", filename);
+    // } else if (S_ISDIR(fs.st_mode)) {
+    //     printf("%s is a directory!\n", filename);
+    // } else {
+    //     printf("%s is some other type of file\n", filename);
+    // }
+
+    if (S_ISBLK(fs.st_mode)) {
+        printf("block special\n");
+    } else if (S_ISCHR(fs.st_mode)) {
+        printf("character special\n");
     } else if (S_ISDIR(fs.st_mode)) {
-        printf("%s is a directory!\n", filename);
+        printf("directory\n");
+    } else if (S_ISFIFO(fs.st_mode)) {
+        printf("named pipe or socket\n");
+    } else if (S_ISREG(fs.st_mode)) {
+        printf("regular file\n");
+    } else if (S_ISLNK(fs.st_mode)) {
+        printf("symbolic link\n");
     } else {
-        printf("%s is some other type of file\n", filename);
+        printf("type unknown\n");
     }
+    
+    printf("Owner permissions:\n");
+    if (fs.st_mode & S_IRUSR) {
+        printf("read\n");
+    }
+    if (fs.st_mode & S_IWUSR) {
+        printf("write\n");
+    }
+    if (fs.st_mode & S_IXUSR) {
+        printf("execute\n");
+    }
+
+    putchar('\n');
+
+    printf("Group permissions:\n");
+    if (fs.st_mode & S_IRGRP) {
+        printf("read\n");
+    }
+    if (fs.st_mode & S_IWGRP) {
+        printf("write\n");
+    }
+    if (fs.st_mode & S_IXGRP) {
+        printf("execute\n");
+    }
+
+    putchar('\n');
+
+    printf("Other permissions:\n");
+    if (fs.st_mode & S_IROTH) {
+        printf("read\n");
+    }
+    if (fs.st_mode & S_IWOTH) {
+        printf("write\n");
+    }
+    if (fs.st_mode & S_IXOTH) {
+        printf("execute\n");
+    }
+
+    putchar('\n');
 
     return 0;
 }

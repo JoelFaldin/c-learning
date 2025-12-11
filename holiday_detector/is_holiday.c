dc7f15b152fbc341bf102afcf87afa80f9140f8b
JoelFaldin <joelfaldin@gmail.com>
2025-12-11 12:23:09 -0300

    feat: add easter function to is_holiday program

--- holiday_detector/is_holiday.c
+++ holiday_detector/is_holiday.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <time.h>
+#include <math.h>
 
 #define FRIDAY 5
 #define MONDAY 1
@@ -15,9 +16,38 @@
 struct holiday {
     int month;
     int day;
+    int year;
     int wday;
     char *name;
 };
+
+int easter(struct holiday *hday) {
+    int Y, a, c, e, h, k, L;
+    double b, d, f, g, i, m, month, day;
+
+    Y = hday -> year;
+    a = Y % 19;
+    b = floor(Y / 100);
+    c = Y % 100;
+    d = floor(b / 4);
+    e = (int)b % 4;
+    f = floor((b + 8) / 25);
+    g = floor((b - f + 1) / 3);
+    h = (19 * a + (int)b - (int)d - (int)g + 15) % 30;
+    i = floor(c / 4);
+    k = c % 4;
+    L = (32 + 2 * e + 2 * (int)i - h - k) % 7;
+    m = floor((a + 11 * h + 22 * L) / 451);
+
+    month = floor((h + L - 7 * m + 114) / 31) - 1;
+    day = ((h + L - 7 * (int)m + 114) % 31) + 1;
+
+    if (hday -> month == month && hday -> day == day) {
+        return 1;
+    } else {
+        return 0;
+    }
+}
 
 int weekend(int holiday, int mday, int wday) {
     if (mday > holiday - 2 && mday < holiday + 2) {
@@ -138,6 +168,12 @@ int is_holiday(struct holiday *h) {
         return r;
     }
 
+    r = easter(h);
+    if (r == 1) {
+        h -> name = n[10];
+        return r;
+    }
+
     return 0;
 }
 
@@ -155,9 +191,10 @@ int main() {
     date.day = today -> tm_mday;
     date.month = today -> tm_mon + 1;
     date.wday = today -> tm_wday;
+    date.year = today -> tm_year + 1900;
 
     // printf("Today is %02d/%02d/%04d", today -> tm_mday, today -> tm_mon + 1, today -> tm_year + 1900);
-    printf("Today is %02d/%02d/%04d", date.day, date.month, today -> tm_year + 1900);
+    printf("Today is %02d/%02d/%04d", date.day, date.month, date.year);
 
     is_h = is_holiday(&date);
 

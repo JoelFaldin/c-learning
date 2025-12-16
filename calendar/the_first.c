4feb846fff9df2351db46cb94ba486924d3f206a
JoelFaldin <joelfaldin@gmail.com>
2025-12-15 23:46:57 -0300

    refactor: moving first day logic into function in the_first program

--- calendar/the_first.c
+++ calendar/the_first.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
 #include <time.h>
 
+int the_first(int wday, int mday) {
+    int first;
+
+    first = wday - (mday % 7) + 1;
+
+    if (first < 0) {
+        first += 7;
+    }
+
+    return first;
+}
+
 int main() {
     const char *weekday[] = {
         "Sunday", "Monday", "Tuesday", "Wednesday",
@@ -13,7 +25,8 @@ int main() {
     time(&now);
     date = localtime(&now);
 
-    first = date -> tm_wday - (date -> tm_wday % 7) + 1;
+    first = the_first(date -> tm_wday, date -> tm_mday);
+
     printf("The first day of this month was on a %s\n", weekday[first]);
 
     return 0;

4b8064a28a70c88a73909536f99a4ceb7f447c3f
JoelFaldin <joelfaldin@gmail.com>
2025-12-18 17:06:39 -0300

    feat: add commify function to odds program

--- lotto_picks/odds.c
+++ lotto_picks/odds.c
@@ -1,19 +1,77 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAX 100
+
+char *commify(float v) {
+    char buff[MAX];
+    char *value = malloc(MAX);
+    int len, j, k;
+
+    snprintf(buff, MAX, "%.0f", v);
+    len = strlen(buff);
+    int i = len - 1;
+
+    k = 0;
+    j = 0;
+    while (i >= 0) {
+        value[k++] = buff[i--];
+        j++;
+
+        if (j % 3 == 0 && i >= 0) {
+            value[k++] = ',';
+        }
+    }
+
+    value[k] = '\0';
+
+    for (int a = 0, b = k - 1; a < b; a++, b--) {
+        char tmp = value[a];
+        value[a] = value[b];
+        value[b] = tmp;
+    }
+
+    return value;
+}
 
 int main() {
-    int ow, ol;
+    int items, draw, x;
+    unsigned long long i, d;
 
-    printf("Chances of happening: ");
-    scanf("%d", &ow);
+    printf("Number of items: ");
+    scanf("%d", &items);
+    if (items == 0) {
+        fprintf(stderr, "You should input a value larger than 0");
+        return 1;
+    }
 
-    printf("Chances of not happening: ");
-    scanf("%d", &ol);
+    printf("Items to draw: ");
+    scanf("%d", &draw);
+    if (draw == 0) {
+        return 1;
+    }
 
-    printf("Your odds of winning are %2.1f%%, or %d:%d\n",
-        (float)ow / (float)(ow + ol) * 100,
-        ow,
-        ol
-    );
+    if (items < draw) {
+        fprintf(stderr, "The number of items drawn should not exceed the number of items available.");
+        return 1;
+    }
+    
+    i = items;
+    d = draw;
+
+    for (x = 1; x < draw; x++) {
+        i *= items - x;
+        d *= draw - x;
+    }
+
+    printf("Your odds of drawing %d ", draw);
+    printf("items from %d are:\n", items);
+    
+    char *result = commify((float)i / (float)d);
+    printf("\t1:%s\n", result);
+
+    free(result);
 
     return 0;
 }
--- lotto_picks/readme.md
+++ lotto_picks/readme.md
@@ -1,12 +1,12 @@
 # Lotto picks
 
-1. Odds of happening and not happening:
+1. Odds of picking items from a list:
 
 ```
 gcc ./lotto_picks/odds.c
 ./a.out
-1
+69
 5
 ```
 
-Result: `Your odds of winning are 16.7%, or 1:5`
+Result: `Your odds of drawing 5 items from 69 are: 1:11,238,513`

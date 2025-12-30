0b10397498ae6820c575988aef46f677b35e0112
JoelFaldin <joelfaldin@gmail.com>
2025-12-29 22:32:56 -0300

    feat: add time_server program

---
+++ Hands_On_Network_Programming_With_C/time_server.c
@@ -0,0 +1,88 @@
+#define _GNU_SOURCE
+
+// Networking headers (linux):
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <unistd.h>
+#include <errno.h>
+
+// Macros for linux:
+#define ISVALIDSOCKET(s) ((s) >= 0)
+#define CLOSESOCKET(s) close(s)
+#define SOCKET int
+#define GETSOCKETERRNO() (errno)
+
+#include <stdio.h>
+#include <string.h>
+#include <time.h>
+
+int main() {
+    struct addrinfo hints;
+    memset(&hints, 0, sizeof(hints));
+
+    hints.ai_family = AF_INET;
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_flags = AI_PASSIVE;
+
+    struct addrinfo *bind_address;
+    getaddrinfo(0, "8080", &hints, &bind_address);
+
+    printf("Creating socket :D\n");
+
+    SOCKET socket_listen;
+    socket_listen = socket(bind_address -> ai_family, bind_address -> ai_socktype, bind_address -> ai_protocol);
+
+    if (!ISVALIDSOCKET(socket_listen)) {
+        fprintf(stderr, "socket() failed. (%d)\n", GETSOCKETERRNO());
+        return 1;
+    }
+
+    printf("Binding socket to local address...\n");
+    if (bind(socket_listen, bind_address -> ai_addr, bind_address -> ai_addrlen)) {
+        fprintf(stderr, "bind() failed. (%d)\n", GETSOCKETERRNO());
+        return 1;
+    }
+
+    freeaddrinfo(bind_address);
+
+    if (listen(socket_listen, 10) < 0) {
+        fprintf(stderr, "listen() failed. (%d)\n", GETSOCKETERRNO());
+        return 1;
+    }
+
+    printf("Listening...\n");
+    if (listen(socket_listen, 10) < 0) {
+        fprintf(stderr, "listen() failed. (%d)\n", GETSOCKETERRNO());
+        return 1;
+    }
+
+    printf("Waiting for connection...\n");
+
+    struct sockaddr_storage client_address;
+    socklen_t client_len = sizeof(client_address);
+
+    SOCKET socket_client = accept(socket_listen, (struct sockaddr*) &client_address, &client_len);
+
+    if (!ISVALIDSOCKET(socket_client)) {
+        fprintf(stderr, "accept() failed. (%d)\n", GETSOCKETERRNO());
+        return 1;
+    }
+
+    printf("Client connected.\n");
+
+    char address_buffer[100];
+    getnameinfo((struct sockaddr*) &client_address, client_len, address_buffer, sizeof(address_buffer), 0, 0, NI_NUMERICHOST);
+    printf("%s\n", address_buffer);
+
+    printf("Reading req...\n");
+
+    char request[1024];
+    int bytes_received = recv(socket_client, request, 1024, 0);
+
+    printf("Received %d bytes\n", bytes_received);
+
+    return 0;
+}
